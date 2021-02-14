23a7b84f2656c29299b8e764e48280273b5aea5e
Pablo Martin Viva <pmviva@gmail.com>
2021-02-13 22:46:49 -0300

    Add UserDTO

---
+++ photo-app-users-api/src/main/java/com/pmviva/photoapp/users/api/dto/UserDTO.java
@@ -0,0 +1,85 @@
+package com.pmviva.photoapp.users.api.dto;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+  /**
+   * The id of the user.
+   */
+  private String id;
+
+  /**
+   * The first name of the user.
+   */
+  private String firstName;
+
+  /**
+   * The last name of the user.
+   */
+  private String lastName;
+
+  /**
+   * The email of the user.
+   */
+  private String email;
+
+  /**
+   * The password of the user.
+   */
+  private String password;
+
+  /**
+   * The encrypted password of the user.
+   */
+  private String encryptedPassword;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEncryptedPassword() {
+    return encryptedPassword;
+  }
+
+  public void setEncryptedPassword(String encryptedPassword) {
+    this.encryptedPassword = encryptedPassword;
+  }
+
+}
